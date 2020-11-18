8ad9afef7901ac1f50c50322d25fd1dcaa12e2ad
Justin Thomas <justhomas@csumb.edu>
2020-11-17 22:05:48 -0800

    finished project

--- js/index.js
+++ js/index.js
@@ -1,28 +1,38 @@
 // js for index.html
 $("document").ready(function (){
-    
-    let apikey = "13513a306e94ec26b9aa01680aa25c85";
-    
-    $("#formZip").on("submit", async function(){
+    let apiKey = "7781ca353e9e747a684659f94e75084e";
+    $("#icon").hide();
+
+    $("#formZip").on("submit", async function(e){
+        e.preventDefault();
+        $("#icon").hide();
         $("#errorZip").hide();
-        $("#dataError").hide();
+        $("#erroApi").hide();        
         
         let zip = $("#inputZip").val()
         if(!checkZip(zip)){
             return;
         }
-        let url = `api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apiKey}`
-        let response = await fetch(url);
-        let data = await response.json();
-        
-        if(data.length < 1){
-            $("#dataError").show();
+        try{
+            let url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apiKey}&units=imperial`;
+            let response = await fetch(url);
+            let data = await response.json();
+            
+            if(data.cod != 200){
+                $("#errorApi").text("Invalid zip code!").show();
+                return;
+            }
+            
+            $("#temp").text(`Tempurature: ${data.main.temp} F`);
+            $("#weather").text(`Weather: ${data.weather[0].description}`);
+            $("#icon").attr("src",`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+            $("#icon").show();
+
+        }catch(err){
+            $("#errorApi").show();
             return;
         }
         
-        $("#temp").text(`Tempurature: ${data.main.temp} F`);
-        $("#weather").text(`Weather: ${data.main.weather}`);
-        $("#icon").attr("src",`http://openweathermap.org/img/wn/${data.weather.icon}.png`)
         
     });// submit
     
@@ -32,8 +42,9 @@ $("document").ready(function (){
      * if false, show error
      */
     function checkZip(zip){
-        if(zip.length() != 5 || zip <= 9999 || zip > 99999){
-            $("#errorZip").show();
+        if(zip.length != 5 || zip <= 9999 || zip > 99999){
+            $()
+            $("#errorZip").text("Invalid zip code!").show();
             return false;
         }
         
